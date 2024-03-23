65131e8fe409fc62f5da7e35705225b647b4684b
Irshad Ahmed <160173156+IrshadAhmedShaikh@users.noreply.github.com>
2024-03-23 23:04:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter your firstnumber", type: "number", name: "firstnumber" },
+  { message: "Enter your secondnumber", type: "number", name: "secondnumber" },
+  {
+    message: "select one of your operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "addition",
+      "subtraction",
+      "multiplication",
+      "divide",
+      "module",
+      "exponentiation",
+    ],
+  },
+]);
+if (answer.operator === "addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "divide") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else if (answer.operator === "module") {
+  console.log(answer.firstnumber % answer.secondnumber);
+} else if (answer.operator === "exponentiation") {
+  console.log(answer.firstnumber ** answer.secondnumber);
+} else {
+  console.log("You Entered Invalid Key");
+}
